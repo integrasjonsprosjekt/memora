fa9f5c95e6fa144bf5cd833b2f0601123c89e0c5
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-09-25 14:44:08 +0200

    refactor: markdown processing into single file

--- frontend/src/components/Markdown/MarkdownRenderer.tsx
+++ frontend/src/components/Markdown/MarkdownRenderer.tsx
@@ -1,10 +1,30 @@
-import {markdownToHtml} from "@/components/Markdown/markdown";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeStringify from "rehype-stringify";
+import rehypeHighlight from "rehype-highlight";
 import styles from "./markdown.module.css";
 import "highlight.js/styles/github-dark.css";
 
 type MarkdownProps = {
   content: string;
 };
+
+export async function markdownToHtml(markdown: string): Promise<string> {
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeHighlight)
+    .use(rehypeStringify)
+    .process(markdown);
+
+  return String(file);
+}
+
 export default async function MarkdownRenderer({content}: MarkdownProps) {
   const html = await markdownToHtml(content);
   return <div className={styles.markdown} dangerouslySetInnerHTML={{ __html: html }} />;
