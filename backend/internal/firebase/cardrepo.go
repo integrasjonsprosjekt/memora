d8f9a11abf0496227ae93ad9b28515d8c3c61b9a
OleMarGlo <om.glomsrud@gmail.com>
2025-10-01 17:10:40 +0200

    chore: structure for dependecy injection for cards

--- backend/internal/firebase/cardrepo.go
+++ backend/internal/firebase/cardrepo.go
@@ -1,1 +1,16 @@
 package firebase
+
+import (
+	"cloud.google.com/go/firestore"
+)
+
+type CardRepository interface {
+}
+
+type FirestoreCardRepo struct {
+	client *firestore.Client
+}
+
+func NewFirestoreCardRepo(client *firestore.Client) *FirestoreCardRepo {
+	return &FirestoreCardRepo{client: client}
+}
---
+++ backend/internal/handlers/cards/cards.go
@@ -0,0 +1,13 @@
+package cards
+
+import (
+	"memora/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateCard(services.CardService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		
+	}
+}
