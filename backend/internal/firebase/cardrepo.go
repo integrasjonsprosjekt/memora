4f0461780067844dbd767d4f794598a0ba7da639
OleMarGlo <om.glomsrud@gmail.com>
2025-10-01 17:35:57 +0200

    feat: update one card with PUT

--- backend/internal/firebase/cardrepo.go
+++ backend/internal/firebase/cardrepo.go
@@ -11,6 +11,7 @@ import (
 type CardRepository interface {
 	CreateCard(ctx context.Context, card any) (string, error)
 	GetCard(ctx context.Context, id string) (map[string]any, error)
+	UpdateCard(ctx context.Context, firestoreUpdates []firestore.Update, id string) error
 }
 
 type FirestoreCardRepo struct {
@@ -21,11 +22,6 @@ func NewFirestoreCardRepo(client *firestore.Client) *FirestoreCardRepo {
 	return &FirestoreCardRepo{client: client}
 }
 
-func (r *FirestoreCardRepo) CreateCard(ctx context.Context, card any) (string, error) {
-	id, _, err := r.client.Collection(config.CardsCollection).Add(ctx, card)
-	return id.ID, err
-}
-
 func (r *FirestoreCardRepo) GetCard(ctx context.Context, id string) (map[string]any, error) {
 	doc, err := r.client.Collection(config.CardsCollection).Doc(id).Get(ctx)
 	if err != nil {
@@ -33,4 +29,25 @@ func (r *FirestoreCardRepo) GetCard(ctx context.Context, id string) (map[string]
 	}
 
 	return doc.Data(), nil
+}
+
+
+func (r *FirestoreCardRepo) CreateCard(ctx context.Context, card any) (string, error) {
+	id, _, err := r.client.Collection(config.CardsCollection).Add(ctx, card)
+	return id.ID, err
+}
+
+func (r *FirestoreCardRepo) UpdateCard(ctx context.Context, firestoreUpdates []firestore.Update, id string) error {
+	docRef := r.client.Collection(config.CardsCollection).Doc(id)
+
+	_, err := docRef.Get(ctx)
+	if err != nil {
+		return errors.ErrInvalidId
+	}
+
+	_, err = docRef.Update(ctx, firestoreUpdates)
+	if err != nil {
+		return err
+	}
+	return nil
 }
