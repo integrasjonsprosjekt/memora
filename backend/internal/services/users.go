dd486a4663888fc93bc314a670f23752a872cd49
OleMarGlo <om.glomsrud@gmail.com>
2025-10-01 17:12:08 +0200

    fix: moved users service to correct file

--- backend/internal/services/users.go
+++ backend/internal/services/users.go
@@ -1,7 +1,11 @@
 package services
 
 import (
+	"context"
+	"memora/internal/errors"
 	"memora/internal/firebase"
+	"memora/internal/models"
+	"memora/internal/utils"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -13,4 +17,46 @@ type UserService struct {
 
 func NewUserService(repo firebase.UserRepository, validate *validator.Validate) *UserService {
 	return &UserService{repo: repo, validate: validate}
+}
+
+func (s *UserService) GetUser(ctx context.Context, id string) (models.User, error) {
+	user, err := s.repo.GetUser(ctx, id)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func (s *UserService) RegisterNewUser(ctx context.Context, user models.CreateUser) (string, error) {
+	id, err := s.repo.AddUser(ctx, user)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func (s *UserService) UpdateUser(
+	ctx context.Context,
+	updateStruct models.PatchUser,
+	id string,
+) error {
+	if err := s.validate.Struct(updateStruct); err != nil {
+		return errors.ErrInvalidUser
+	}
+
+	update, err := utils.StructToUpdate(updateStruct)
+	if err != nil {
+		return errors.ErrInvalidUser
+	}
+
+	err = s.repo.UpdateUser(ctx, update, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) DeleteUser(ctx context.Context, id string) error {
+	return s.repo.DeleteUser(ctx, id)
 }
