c71c3d13dd9ccbc0ce1b7c92f96c37c082dd9491
Runar Fredagsvik <i@runar.ch>
2025-10-06 18:41:30 +0200

    feat: display breadcrumbs using parallel routes
    Implementation adapted from https://www.openstatus.dev/blog/dynamic-breadcrumb-nextjs

---
+++ frontend/src/app/@breadcrumb/[...all]/page.tsx
@@ -0,0 +1,62 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import React from 'react';
+import type { ReactElement } from 'react';
+
+// List of routes that should not be linked
+const NON_LINKABLE_ROUTES = new Set(['/decks']);
+
+function isValidRoute(path: string): boolean {
+  return !NON_LINKABLE_ROUTES.has(path);
+}
+
+export default async function BreadcrumbSlot(props: { params: Promise<{ all: string[] }> }) {
+  const params = await props.params;
+  const breadcrumbItems: ReactElement[] = [];
+  let breadcrumbPage: ReactElement = <></>;
+
+  for (let i = 0; i < params.all.length; i++) {
+    const route = params.all[i];
+    const href = `/${params.all.slice(0, i + 1).join('/')}`;
+    const isLast = i === params.all.length - 1;
+    const shouldLink = isValidRoute(href);
+
+    if (isLast) {
+      breadcrumbPage = (
+        <BreadcrumbItem>
+          <BreadcrumbPage className="capitalize">{route}</BreadcrumbPage>
+        </BreadcrumbItem>
+      );
+    } else {
+      breadcrumbItems.push(
+        <React.Fragment key={href}>
+          <BreadcrumbItem>
+            {shouldLink ? (
+              <BreadcrumbLink href={href} className="capitalize">
+                {route}
+              </BreadcrumbLink>
+            ) : (
+              <BreadcrumbPage className="capitalize">{route}</BreadcrumbPage>
+            )}
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+        </React.Fragment>,
+      );
+    }
+  }
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {breadcrumbItems}
+        {breadcrumbPage}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
