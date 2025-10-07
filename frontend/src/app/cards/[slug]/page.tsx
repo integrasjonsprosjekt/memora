02f17f5a54dcc9267e479ff54b7a4fc7610009b3
Runar Fredagsvik <i@runar.ch>
2025-10-07 21:09:28 +0200

    chore: load environment variables

--- frontend/src/app/cards/[slug]/page.tsx
+++ frontend/src/app/cards/[slug]/page.tsx
@@ -1,6 +1,8 @@
 export default async function CardPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
 
+  const card = await fetch(`${process.env.API_URI}/v1/cards/${slug}`).then((res) => res.json());
+
   return (
     <div>
       <h1>Viewing card {slug}</h1>
