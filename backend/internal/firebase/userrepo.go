fa3ced8ac3b41666f6fb0b5db1366a12ef543162
OleMarGlo <om.glomsrud@gmail.com>
2025-10-08 14:44:06 +0200

    feat: first draft for function to fetch shared emails

--- backend/internal/firebase/userrepo.go
+++ backend/internal/firebase/userrepo.go
@@ -15,6 +15,7 @@ type UserRepository interface {
 	AddUser(ctx context.Context, u models.CreateUser) (string, error)
 	GetUser(ctx context.Context, id string) (models.User, error)
 	GetDecksOwned(ctx context.Context, id string) ([]models.DisplayDeck, error)
+	GetDecksShared(ctx context.Context, id string) ([]models.DisplayDeck, error)
 	UpdateUser(ctx context.Context, firestoreUpdates []firestore.Update, id string) error
 	DeleteUser(ctx context.Context, id string) error
 }
@@ -48,6 +49,40 @@ func (r *FirestoreUserRepo) GetDecksOwned(ctx context.Context, id string) ([]mod
 
 	iter := r.client.Collection(config.DecksCollection).
 		Where("owner_id", "==", id).
+		Documents(ctx)
+
+	for {
+		doc, err := iter.Next()
+
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		var deck models.DisplayDeck
+		if err := doc.DataTo(&deck); err != nil {
+			return nil, err
+		}
+
+		deck.ID = doc.Ref.ID
+		decks = append(decks, deck)
+	}
+
+	return decks, nil
+}
+
+func (r *FirestoreUserRepo) GetDecksShared(ctx context.Context, id string) ([]models.DisplayDeck, error) {
+	var decks []models.DisplayDeck
+
+	user, err := utils.FetchByID[models.User](r.client, ctx, config.UsersCollection, id)
+	if err != nil {
+		return nil, err
+	}
+
+	iter := r.client.Collection(config.DecksCollection).
+		Where("shared_emails", "array-contains", user.Email).
 		Documents(ctx)
 
 	for {
