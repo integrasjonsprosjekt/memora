c8fa0564371b693fa7d645067980cbab643ddc47
OleMarGlo <om.glomsrud@gmail.com>
2025-10-15 19:01:18 +0200

    refactor: user endpoint gets ID and email from Auth provider

--- backend/internal/handlers/users/users.go
+++ backend/internal/handlers/users/users.go
@@ -25,7 +25,7 @@ func GetUser(userRepo *services.UserService) gin.HandlerFunc {
 			c.Status(http.StatusUnauthorized)
 			return
 		}
-		
+
 		user, err := userRepo.GetUser(c.Request.Context(), id, filter)
 		if errors.HandleError(c, err) {
 			return
@@ -107,11 +107,16 @@ func CreateUser(userRepo *services.UserService) gin.HandlerFunc {
 func PatchUser(userRepo *services.UserService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var updates models.CreateUser
-		id := c.GetString("uid")
 		if err := c.ShouldBindBodyWithJSON(&updates); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": "invalid JSON",
 			})
+			return
+		}
+
+		id, err := utils.GetUID(c)
+		if err != nil {
+			c.Status(http.StatusUnauthorized)
 			return
 		}
 
@@ -133,9 +138,13 @@ func PatchUser(userRepo *services.UserService) gin.HandlerFunc {
 // @Router /api/v1/users/{id} [delete]
 func DeleteUser(userRepo *services.UserService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		id := c.GetString("uid")
+		id, err := utils.GetUID(c)
+		if err != nil {
+			c.Status(http.StatusUnauthorized)
+			return
+		}
 
-		err := userRepo.DeleteUser(c.Request.Context(), id)
+		err = userRepo.DeleteUser(c.Request.Context(), id)
 		if errors.HandleError(c, err) {
 			return
 		}
