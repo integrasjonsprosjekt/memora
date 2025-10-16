cd7cf4911e9c95e2533931eebea4def37e269d85
OleMarGlo <om.glomsrud@gmail.com>
2025-10-16 15:30:11 +0200

    chore: create a user in firebase emulator

--- backend/internal/integration/api_test.go
+++ backend/internal/integration/api_test.go
@@ -1,11 +1,14 @@
 package integration_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"log"
 	"memora/internal/firebase"
 	"memora/internal/router"
 	"memora/internal/services"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -35,4 +38,24 @@ func PerformRequest(r *gin.Engine, method, path string, body io.Reader) *httptes
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w
+}
+
+func CreateTestUser(r *gin.Engine, t *testing.T) string {
+	url := "http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signUp?key=any"
+	payload := map[string]string{
+		"email":             "test@user.com",
+		"password":          "verysecurepassword",
+		"returnSecureToken": "true",
+	}
+	body, _ := json.Marshal(payload)
+	resp, err := http.Post(url, "application/json", bytes.NewReader(body))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	t.Log(result)
+	return result["idToken"].(string)
 }
