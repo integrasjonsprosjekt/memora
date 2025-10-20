038d691889fef7de7f44f8dc57f8ed398193b510
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-10-20 17:56:30 +0200

    feat: add delete deck

---
+++ frontend/src/components/delete-deck-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { deleteDeck } from "@/app/api";
+import { useRouter } from "next/navigation";
+
+export function DeleteDeckButton({deckId}: {deckId: string}) {
+  const router = useRouter();
+  async function handler({deckId}: {deckId: string}) {
+    const res = await deleteDeck(deckId);
+    if (res.success) {
+      alert("Deck deleted successfully");
+      router.push(`/`);
+    } else {
+      alert("Failed to delete deck");
+    }
+  }
+  return (
+    <>
+      <Button
+        className="bg-destructive mx-20"
+        onClick={() => handler({deckId})}
+      >
+        Delete card
+      </Button>
+    </>
+  );
+}
