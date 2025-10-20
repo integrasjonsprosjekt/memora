62e961ab1e441b03ee889a5173c69c0576170ac9
Christian Hegna Langedok <christian.hegna.langedok@gmail.com>
2025-10-20 17:30:23 +0200

    feat: add edit deck

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -2,6 +2,12 @@
 
 import { CardType } from "@/types/cards";
 
+interface UpdateDeckPayload {
+  title?: string;
+  addedEmails?: string[];
+  removedEmails?: string[];
+}
+
 export async function createCard(id: string, type: CardType, data: Record<string, unknown>) {
   const body = { type, ...data };
 
@@ -66,7 +72,7 @@ export async function deleteCard(deck_id: string, card_id: string) {
 
 export async function createDeck(title: string, shared_emails?: string[]) {
   const body = {
-    owner_id: "xHoY659nXFVpWBdc93NH", // TODO: Remove hardcoded user ID
+    owner_id: "zb8FOCiyYCfXmqpPpxaU7Kjm5p92", // TODO: Remove hardcoded user ID
     title,
     shared_emails
   };
@@ -84,4 +90,55 @@ export async function createDeck(title: string, shared_emails?: string[]) {
   }
 
   return { success: true, data: result };
+}
+
+export async function updateDeck(deck_id: string, payload: UpdateDeckPayload) {
+
+  const responseTitle = await fetch(`${process.env.API_URI}/v1/decks/${deck_id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({title: payload.title}),
+  });
+
+  const resultTitle = await responseTitle.json();
+
+  if (!responseTitle.ok) {
+    return { success: false, message: resultTitle.error || "Failed to update deck title" };
+  }
+
+  const addBody = {
+    opp: "add",
+    shared_emails: payload.addedEmails
+  }
+  const removeBody = {
+    opp: "remove",
+    shared_emails: payload.removedEmails
+  }
+
+  const emailResponses: Response[] = []
+
+  if (payload.addedEmails?.length) {
+    emailResponses.push(await fetch(`${process.env.API_URI}/v1/decks/${deck_id}/emails`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(addBody),
+    }));
+  }
+  if (payload.removedEmails?.length) {
+    emailResponses.push(await fetch(`${process.env.API_URI}/v1/decks/${deck_id}/emails`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(removeBody),
+    }));
+  }
+
+  let resultEmails;
+  for (const res of emailResponses) {
+    resultEmails = await res.json().catch(() => []);
+    if (!res.ok) {
+      return { success: false, message: resultEmails.error || "Failed to update deck emails" };
+    }
+  }
+
+  return { success: true, data: resultTitle + resultEmails };
 }
