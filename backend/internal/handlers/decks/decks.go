4182762d6abcc863429186f7128ec42024ef193f
OleMarGlo <om.glomsrud@gmail.com>
2025-11-06 15:23:49 +0100

    chore: formatting

--- backend/internal/handlers/decks/decks.go
+++ backend/internal/handlers/decks/decks.go
@@ -70,7 +70,7 @@ func GetDeck(deckRepo *services.DeckService) gin.HandlerFunc {
 			errors.HandleError(c, errors.ErrUnauthorized)
 			return
 		}
-		
+
 		deck, err := deckRepo.GetOneDeck(c.Request.Context(), deckID, filter)
 		if errors.HandleError(c, err) {
 			return
