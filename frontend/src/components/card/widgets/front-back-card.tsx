3a37be9561d4e81ffb075b54e79000e0d0e407fb
Runar Fredagsvik <i@runar.ch>
2025-11-06 21:52:23 +0100

    feat: interactive front-back card

--- frontend/src/components/card/widgets/front-back-card.tsx
+++ frontend/src/components/card/widgets/front-back-card.tsx
@@ -1,30 +1,10 @@
-'use client';
-
-import { JSX, useState } from 'react';
-import styles from '../card.module.css';
+import { JSX } from 'react';
 import { FrontBackCard as FrontBackCardType } from '@/types/card';
 import { CardComponentProps } from '../types';
-import { ClientMarkdownRenderer } from '@/components/markdown/client-markdown-renderer';
+import { FrontBackCardInteractive } from './front-back-card-interactive';
 
 export function FrontBackCard({ card, className }: CardComponentProps<FrontBackCardType>): JSX.Element {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  return (
-    <div
-      className={`${styles.front_back} ${isFlipped ? styles.front_back_flipped : ''} ${className}`}
-      onClick={() => setIsFlipped(!isFlipped)}
-    >
-      <div className={styles.front}>
-        <ClientMarkdownRenderer>{card.front}</ClientMarkdownRenderer>
-      </div>
-
-      <hr className="border-border tap-highlight-transparent my-5 w-full border-t border-dashed" />
-
-      <div className={styles.back}>
-        <ClientMarkdownRenderer>{card.back}</ClientMarkdownRenderer>
-      </div>
-    </div>
-  );
+  return <FrontBackCardInteractive card={card} className={className} />;
 }
 
 export function FrontBackCardThumbnail({ card, className }: CardComponentProps<FrontBackCardType>): JSX.Element {
