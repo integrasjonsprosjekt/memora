e76ac3ec58c4a8378b98694147a4013cef5dec2f
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-11-07 13:28:13 +0100

    chore: remove unused code

--- frontend/src/lib/firebase/serverApp.ts
+++ frontend/src/lib/firebase/serverApp.ts
@@ -1,17 +1,11 @@
 import { cache } from 'react';
-import { FirebaseApp, getApp, getApps, initializeApp, initializeServerApp } from 'firebase/app';
+import { initializeServerApp } from 'firebase/app';
 import { getAuth, User } from 'firebase/auth';
 import { firebaseConfig } from './app';
 import { cookies } from 'next/headers';
 
-function getOrInitApp(): FirebaseApp {
-  const apps = getApps();
-  return apps.length ? getApp() : initializeApp(firebaseConfig);
-}
-
 export const getUser = cache(async (): Promise<User | null> => {
   const idToken = (await cookies()).get('__session')?.value;
-  const baseApp = getOrInitApp();
   const serverApp = initializeServerApp(firebaseConfig, { authIdToken: idToken });
   const auth = getAuth(serverApp);
 
